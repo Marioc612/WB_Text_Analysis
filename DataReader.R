8fb8d5b5ebcad5c5de68e5127890f40ec10a98ce
Camilo-Betancur <camilo.bj@tutanota.com>
2022-09-13 19:16:02 -0500

    Added function for importing saved json files

--- DataReader.R
+++ DataReader.R
@@ -4,9 +4,8 @@ library(glue)
 
 
 extract <- function(folder_path, absolute_path = FALSE) {
-  cwd <- dirname(rstudioapi::getSourceEditorContext()$path)
-
   if (absolute_path == FALSE) {
+    cwd <- dirname(rstudioapi::getSourceEditorContext()$path)
     path <- paste(cwd, folder_path, sep = "/")
   }
 
@@ -96,8 +95,26 @@ tidify <- function(df, get_top = 1, export_json = FALSE, json_name = NULL) {
 
   if (export_json == TRUE) {
     cwd <- dirname(rstudioapi::getSourceEditorContext()$path)
-    write(toJSON(tibblist), file = glue("{cwd}/Saves/Tidy/{json_name}.json"))
+    write(jsonlite::toJSON(tibblist),
+      file = glue("{cwd}/Saves/Tidy/{json_name}.json")
+    )
   }
 
   return(tibblist)
+}
+
+
+import_json <- function(json_path, absolute_path = FALSE) {
+  if (absolute_path == FALSE) {
+    cwd <- dirname(rstudioapi::getSourceEditorContext()$path)
+    path <- paste(cwd, json_path, sep = "/")
+  }
+
+  if (absolute_path == TRUE) {
+    path <- json_path
+  }
+
+  return(
+    as_tibble(import_json()(path))
+  )
 }
